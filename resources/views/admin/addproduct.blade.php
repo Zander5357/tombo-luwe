a3ed91ab2a5b202d2126cd15a2c18d35fced109e
Zander <bagasgnwn@gmail.com>
2024-03-24 20:27:41 +0700

    Add project files to repository

---
+++ resources/views/admin/addproduct.blade.php
@@ -0,0 +1,147 @@
+  <?php
+  session_start();
+  include("../db.php");
+
+
+  if (isset($_POST['btn_save'])) {
+    $product_name = $_POST['product_name'];
+    $details = $_POST['details'];
+    $price = $_POST['price'];
+    $product_type = $_POST['product_type'];
+    $brand = $_POST['brand'];
+    $tags = $_POST['tags'];
+
+    //picture coding
+    $picture_name = $_FILES['picture']['name'];
+    $picture_type = $_FILES['picture']['type'];
+    $picture_tmp_name = $_FILES['picture']['tmp_name'];
+    $picture_size = $_FILES['picture']['size'];
+
+    if ($picture_type == "image/jpeg" || $picture_type == "image/jpg" || $picture_type == "image/png" || $picture_type == "image/gif") {
+      if ($picture_size <= 50000000)
+
+        $pic_name = time() . "_" . $picture_name;
+      move_uploaded_file($picture_tmp_name, "../product_images/" . $pic_name);
+
+      pg_query($con, "INSERT into products (product_cat, product_brand,product_title, product_price, product_desc, product_image,product_keywords) values ('$product_type','$brand','$product_name','$price','$details','$pic_name','$tags')") or die("query incorrect");
+
+      header("location: sumit_form.php?success=1");
+    }
+
+    pg_close($con);
+  }
+  include "sidenav.php";
+  include "topheader.php";
+  ?>
+  <!-- End Navbar -->
+  <div class="content">
+    <div class="container-fluid">
+      <form action="" method="post" type="form" name="form" enctype="multipart/form-data">
+        <div class="row">
+
+
+          <div class="col-md-7">
+            <div class="card">
+              <div class="card-header card-header-primary">
+                <h5 class="title">Add Product</h5>
+              </div>
+              <div class="card-body">
+
+                <div class="row">
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label>Product Title</label>
+                      <input type="text" id="product_name" required name="product_name" class="form-control">
+                    </div>
+                  </div>
+                  <div class="col-md-4">
+                    <div class="">
+                      <label for="">Add Image</label>
+                      <input type="file" name="picture" required class="btn btn-fill btn-success" id="picture">
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label>Description</label>
+                      <textarea rows="4" cols="80" id="details" required name="details" class="form-control"></textarea>
+                    </div>
+                  </div>
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label>Pricing</label>
+                      <input type="text" id="price" name="price" required class="form-control">
+                    </div>
+                  </div>
+                </div>
+
+
+
+              </div>
+
+            </div>
+          </div>
+          <div class="col-md-5">
+            <div class="card">
+              <div class="card-header card-header-primary">
+                <h5 class="title">Categories</h5>
+              </div>
+              <div class="card-body">
+
+                <div class="row">
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label>Product Category</label>
+                      <select name="product_type" id="" product_type>
+                        <option value="1">Nugget</option>
+                        <option value="2">Sosis</option>
+                        <option value="3">Siomay</option>
+                        <option value="4">Bakso</option>
+                        <option value="5">Daging</option>
+                        <option value="6">Kentang</option>
+                        <option value="7">Dimsum</option>
+                      </select>
+
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label for="">Product Brand</label>
+                      <select name="brand" id="" brand>
+                        <option value="1">Champ</option>
+                        <option value="2">So Good</option>
+                        <option value="3">Sunny Gold</option>
+                        <option value="4">Kanzler</option>
+                        <option value="5">Belfoods</option>
+                        <option value="6">Fiesta</option>
+                        <option value="7">UMKM</option>
+                      </select>
+                    </div>
+                  </div>
+
+
+                  <div class="col-md-12">
+                    <div class="form-group">
+                      <label>Product Keywords</label>
+                      <input type="text" id="tags" name="tags" required class="form-control">
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+              <div class="card-footer">
+                <button type="submit" id="btn_save" name="btn_save" required class="btn btn-fill btn-primary">Update Product</button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </form>
+
+    </div>
+  </div>
+  <?php
+  include "footer.php";
+  ?>
---
+++ resources/views/db.php
@@ -0,0 +1,8 @@
+<?php
+$db_username = "postgres";
+$db_password = "robyarjuna17";
+$con = pg_connect("host=localhost dbname=onlineshop user=$db_username password=$db_password");
+// Check connection
+if (!$con) {
+    die("Connection failed: ");
+}
